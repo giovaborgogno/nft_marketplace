b911d8db859039b29ca2ea4614115b7c71945c37
Giovanni Borgogno <giovaborgogno@gmail.com>
2023-10-18 20:41:04 -0300

    responsive hot fix

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -17,8 +17,8 @@ const NFTCard = ({ nft, onProfilePage }) => {
   // console.log(img)
   return (
     <Link href={{ pathname: '/nft-details', query: nft }}>
-      <div className="flex-1 min-w-225 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 sm:mx-0 minlg:m-8 sm:my-2  cursor-pointer shadow-md">
-        <div className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
+      <div className="flex-1 min-w-225 max-w-max sm:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 sm:mx-0 minlg:m-8 sm:my-2  cursor-pointer shadow-md">
+        <div className="relative w-full h-52 sm:h-60 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
           <Image src={nft.image || images.nft2} layout="fill" objectFit="cover" alt="nft01" />
         </div>
         <div className="mt-3 flex flex-col">
